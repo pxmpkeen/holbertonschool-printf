d8206fac787a81a5fa0864e9e782f2788043823d
pxmpkeen <pxmpkeen@gmail.com>
2023-11-27 06:42:24 -0800

    0_printf.c

--- 0_printf.c
+++ 0_printf.c
@@ -9,43 +9,35 @@
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	int condition;
-	int i = 0, pc = 0, sl = 0;
-	char endl = 10;
-	char character, *string;
-
+	int condition_p, condition_b, i = 0, pc = 0, sl = 0;
+	char endl = 10, c, *s;
 
 	va_start(ap, format);
 	if (!format)
 		return (0);
 	while (format[i])
 	{
-		condition = (format[i] == '%');
-		if (condition && format[i + 1] == 'c')
+		condition_b = (format[i] == 92)
+		condition_p = (format[i] == '%');
+		if (condition_p && format[i + 1] == 'c')
 		{
-			character = (char)va_arg(ap, int);
-			write(1, &character, 1), i += 2, pc++;
+			c = (char)va_arg(ap, int);
+			write(1, &c, 1), i += 2, pc++;
 		}
-		else if (condition && format[i + 1] == 's')
+		else if (condition_p && format[i + 1] == 's')
 		{
-			string = va_arg(ap, char *);
-			while (string[sl])
+			s = va_arg(ap, char *);
+			while (s[sl])
 				sl++;
-			write(1, string, sl);
-			i += 2, pc += sl, sl = 0;
+			write(1, s, sl), i += 2, pc += sl, sl = 0;
 		}
-		else if (condition && format[i + 1] == '%')
-		{
-			character = '%';
-			write(1, &character, 1), i += 2, pc++;
-		}
+		else if (condition_p && format[i + 1] == '%')
+			c = '%', write(1, &c, 1), i += 2, pc++;
+		else if (condition_b && format[i + 1] == 'n')
+			write(1, &endl, 1), i += 2, pc++;
 		else
-		{
-			character = format[i];
-			write(1, &character, 1), i++, pc++;
-		}
+			c = format[i], write(1, &c, 1), i++, pc++;
 	}
-	write(1, &endl, 1);
 	va_end(ap);
 	return (pc);
 }
