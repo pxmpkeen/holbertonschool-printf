3fbbb4efa0204c09b72443b854cdca696f8e4b2b
pxmpkeen <pxmpkeen@gmail.com>
2023-12-01 03:38:44 -0800

    add

--- printf.c
+++ printf.c
@@ -153,11 +153,11 @@ char formatter(char c)
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	char c, *s;
+	char c;
 	int condition_spec, i = 0, pc = 0, l;
 	spec_t specs[] = {
 		{' ', spec_placeholder},
-		{'b', spec_binary},
+		{'b', spec_placeholder},
 		{'c', spec_char},
 		{'d', spec_int},
 		{'#', spec_placeholder},
@@ -199,44 +199,4 @@ int _printf(const char *format, ...)
 	}
 	va_end(ap);
 	return (pc);
-}
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-	int len;
-	int len2;
-	unsigned int ui;
-	void *addr;
-
-	len = _printf("Let's try to printf a simple sentence.\n");
-	len2 = printf("Let's try to printf a simple sentence.\n");
-	ui = (unsigned int)INT_MAX + 1024;
-	addr = (void *)0x7ffe637541f0;
-	_printf("Length:[%d, %i]\n", len, len);
-	printf("Length:[%d, %i]\n", len2, len2);
-	_printf("Negative:[%d]\n", -762534);
-	printf("Negative:[%d]\n", -762534);
-	_printf("Unsigned:[%u]\n", ui);
-	printf("Unsigned:[%u]\n", ui);
-	_printf("Unsigned octal:[%o]\n", ui);
-	printf("Unsigned octal:[%o]\n", ui);
-	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-	_printf("Character:[%c]\n", 'H');
-	printf("Character:[%c]\n", 'H');
-	_printf("String:[%s]\n", "I am a string !");
-	printf("String:[%s]\n", "I am a string !");
-	_printf("Address:[%p]\n", addr);
-	printf("Address:[%p]\n", addr);
-	len = _printf("Percent:[%%]\n");
-	len2 = printf("Percent:[%%]\n");
-	_printf("Len:[%d]\n", len);
-	printf("Len:[%d]\n", len2);
-	_printf("Unknown:[%r]\n");
-	printf("Unknown:[%r]\n");
-	return (0);
 }
